886e7b97e38083fe50ea65a1f64bfecd2437be78
Patrik Polyak <ppo@vizrt.com>
2020-04-06 22:17:20 +0200

    Changed Perl regexp so that it works with default OSX binaries as well, fixes #1

--- fetch_pdfs.sh
+++ fetch_pdfs.sh
@@ -4,4 +4,4 @@ pdf_dir="pdf"
 site_data=$(curl -s 'https://www.google.com/covid19/mobility/')
 [ ! -d "$pdf_dir" ] && mkdir -p "$pdf_dir"
 cd "$pdf_dir"
-(grep 'https://www.gstatic.com/covid19/mobility/' | grep -Po '".*"' | xargs -n1 wget -nv) <<< "$site_data"
+(grep 'https://www.gstatic.com/covid19/mobility/' | perl -nle'print $& while m{".*"}g' | xargs -n1 wget -nv) <<< "$site_data"
